16dea4f6a21cc54936f400740b9b1c8b00c9c833
neeajgupta3325-glitch <neeajgupta3325@gmail.com>
2026-06-18 12:20:20 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 4325135
+let accountEmail = "hitesh@google.com"
+var accountPassword = "472895"
+
+console.table([accountId,accountEmail,accountPassword]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,9 @@
+let age = 19
+let name = "hitesh"
+console.table([age, name])
+console.log(typeof age);
+console.log(typeof name);
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof BigInt)
