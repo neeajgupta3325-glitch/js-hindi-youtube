b9d5b423187d7935524b4acff9a0ad56f51c2334
neeajgupta3325-glitch <neeajgupta3325@gmail.com>
2026-06-18 12:51:03 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = false
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33"=> 33
+// "33as" => NaN -- not a number
+// Hitesh => NaN
+// null => 0
+// true => 1
+// false => 0
+
+let isLoggedIn = ""
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
